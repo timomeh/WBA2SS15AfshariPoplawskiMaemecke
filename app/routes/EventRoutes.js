d78e467018e3bd16782e31cfb6459e9a4412fa51
plsr <christianpoplawski@gmail.com>
2015-06-07 11:28:04 +0200

    Basic routes for Events implemented

--- app/routes/EventRoutes.js
+++ app/routes/EventRoutes.js
@@ -8,28 +8,54 @@ var db = redis.createClient();
 // =================
 
 router.post('/', function (req, res) {
-  var id = events.length;
+  db.incr('eventIDs', function (err, id) {
+    if(err) return res.status(500).send('Error while incrementing ID in Database');
+    var event = req.body;
+    
+    // Set id of event to consitent one calculated by db
+    event.id = id;
 
-  var event = {};
-  event.id = id;
-  event.name = req.body.name;
-  event.time = req.body.time;
-  events.push(event);
-  res.json(event);
+    db.set('event:' +id, JSON.stringify(event), function (err, rep) {
+      if(err) return res.status(500).send('Error while writing to Database');
+      res.json(event);
+    })
+  });
 });
 
-router.get('/', function (req, res) {
-  res.json(events);
-});
 
 router.get('/:id', function (req, res) {
-  res.json(events[req.params.id]);
+  var eventID = req.params.id;
+
+  db.get('event:' +eventID, function(err, rep) {
+    if(err) return res.status(500).send('Error while reading from database.');
+    if(rep == null) return res.status(404).send('No event with the id ' +eventID +' was found in the database.');
+    res.json(JSON.parse(rep));
+  });
 });
 
 router.put('/:id', function (req, res) {
-  events[req.params.id].name = req.body.name;
-  events[req.params.id].time = req.body.time;
-  res.json(events[req.params.id]);
+  var eventID = req.params.id;
+  var event = req.body;
+  event.id = eventID;
+
+  db.get('event:' +eventID, function(err, rep){
+    if(rep == null) return res.status(404).send('No event with the id ' +eventID +' was found in the database.');
+
+    db.set('event:' +eventID, JSON.stringify(event), function(err, rep) {
+      if(err) return res.status(500).send('Error while writing to Database');
+      res.json(event);
+    });
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  var eventID = req.params.id;
+  
+  // Best Practice? Return deleted key/value?
+  db.del('event:' +eventID, function(err, rep) {
+    if(err) return res.status(500).send('Error while deleting from Database');
+    return res.status(200).send('Successfully deleted event with the id ' +eventID);
+  })
 });
 
 
