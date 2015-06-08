1e6d35e1d782c83b085cc7d83d704676983b368c
Timo Mämecke <timo@maemecke.com>
2015-06-08 11:35:33 +0200

    pushed view engine

--- client.js
+++ client.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var cons = require('consolidate');
 
 // Express instance
 var app = express();
 app.set('port', process.env.PORT || 8000);
 
+// assign the ejs engine to .ejs files
+app.engine('ejs', cons.ejs);
+
+// set .html as the default extension
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/frontend/views');
+
 // Routes are in app/routes.js
-app.use('/api', require('./app/RouteMap'));
+app.use('/', require('./frontend/RouteMap'));
 
 
 // Start the server
