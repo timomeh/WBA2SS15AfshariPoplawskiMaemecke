db28951f2526bf2e8d80bbbbfb37e0510add1c64
plsr <christianpoplawski@gmail.com>
2015-06-22 09:35:19 +0200

    Some basic frontend stuff

--- frontend/RouteMap.js
+++ frontend/RouteMap.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res) {
   res.render('test', { name: 'Welt' });
 })
 
+router.get('/events', function(req, res) {
+  res.render('event-main', { name: 'Welt' });
+})
+
 module.exports = router;
